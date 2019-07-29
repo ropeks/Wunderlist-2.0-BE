52ff0b958d1f58a6c7d099f4a23362c0f15d6783
ropeks <ropeksmilioner@gmail.com>
2019-07-29 15:56:30 +0200

    add knex migrations and seeds

---
+++ data/migrations/20190729151831_usersAndTodosTable.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', tbl => {
+        tbl.increments();
+        tbl.string('username', 20)
+            .notNullable()
+            .unique();
+        tbl.string('password', 30)
+            .notNullable();
+    })
+    .createTable('todos', tbl => {
+        tbl.increments();
+        tbl.integer('user_id')
+            .unsigned()
+            .references('users.id')
+        tbl.string('caption', 50)
+            .notNullable();
+        tbl.string('description', 500)
+            .notNullable();
+        tbl.date('due_date');
+        tbl.boolean('completed');
+        tbl.boolean('deleted');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('todos');
+};
