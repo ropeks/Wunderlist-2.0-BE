99fe57de26fa64a5d4f1d1d2d7559fa55a2697d8
ropeks <ropeksmilioner@gmail.com>
2019-07-30 13:16:32 +0200

    add getById and delete endpoints

--- data/helpers/todos-model.js
+++ data/helpers/todos-model.js
@@ -1,9 +1,23 @@
 const db = require('../dbConfig.js');
 
-function get(id) {
-    return db('todos').where('user_id', id);
+function get(userID) {
+    return db('todos').where('user_id', userID);
+}
+
+function getById(userID, id) {
+    return get(userID)
+        .where({ id })
+        .first();
+}
+
+function remove(userID, id) {
+    return get(userID)
+        .where({ id })
+        .del();
 }
 
 module.exports = {
-    get
+    remove,
+    get,
+    getById
 };
--- router/todos.js
+++ router/todos.js
@@ -14,4 +14,31 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const todo = await db.getById(res.locals.id, req.params.id);
+        if (todo) {
+            res.status(200).json(todo);
+        } else {
+            res.status(401).json({ message: 'You are not allowed to see this todo, as it belongs to someone else...' });
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const todo = await db.getById(res.locals.id, req.params.id);
+        if (todo) {
+            await db.remove(res.locals.id, req.params.id);
+            res.status(200).json({ message: 'Todo has been deleted.' });
+        } else {
+            res.status(401).json({ message: 'You are not allowed to delete this todo, as it belongs to someone else...' });
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+});
+
 module.exports = router;
