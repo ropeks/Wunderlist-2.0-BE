3191bb08f2153ed49c4bade060d17d328caef55f
ropeks <ropeksmilioner@gmail.com>
2019-07-31 18:52:02 +0200

    add more tests for todos-model

--- data/helpers/todos-model.spec.js
+++ data/helpers/todos-model.spec.js
@@ -15,8 +15,8 @@ const dummyTodo2 = {
     "caption": "test todo 2",
     "description": "test test test test test test test test test 2",
     "due_date": [2019, 8, 2, 12, 2],
-    "completed": 1,
-    "deleted": 1
+    "completed": 0,
+    "deleted": 0
 };
 
 describe('Todos Model', () => {
@@ -37,7 +37,7 @@ describe('Todos Model', () => {
         it('should return added todo', async () => {
             await Todos.add(1, dummyTodo);
             const todos = await db('todos');
-            expect(todos[0].caption).toBe('test todo');
+            expect(todos[0].caption).toBe(dummyTodo.caption);
         });
 
     });
@@ -59,7 +59,7 @@ describe('Todos Model', () => {
             await Todos.add(1, dummyTodo);
             await Todos.add(1, dummyTodo2);
             const todo = await Todos.getById(1, 2);
-            expect(todo.caption).toBe('test todo 2');
+            expect(todo.caption).toBe(dummyTodo2.caption);
         });
 
     });
@@ -72,26 +72,42 @@ describe('Todos Model', () => {
             const todo = await Todos.remove(1, 1);
             const todos = await db('todos');
             expect(todos).toHaveLength(1);
-            expect(todos[0].caption).toBe('test todo 2');
+            expect(todos[0].caption).toBe(dummyTodo2.caption);
         });
 
     });
 
     describe('update(userID, id, changes)', () => {
 
-        
+        it('should update desired todo', async () => {
+            await Todos.add(1, dummyTodo);
+            await Todos.update(1, 1, { ...dummyTodo2 });
+            const todos = await db('todos');
+            expect(todos[0].caption).toBe(dummyTodo2.caption);
+        });
 
     });
 
     describe('getDeleted(userID)', () => {
 
-        
+        it('should display deleted todos', async () => {
+            await Todos.add(1, dummyTodo);
+            await Todos.add(1, dummyTodo2);
+            const deletedTodos = await Todos.getDeleted(1);
+            expect(deletedTodos).toHaveLength(1);
+        });
 
     });
 
     describe('getCompleted(userID)', () => {
 
-        
+        it('should display completed todos', async () => {
+            await Todos.add(1, dummyTodo);
+            await Todos.add(1, dummyTodo2);
+            const completedTodos = await Todos.getCompleted(1);
+            expect(completedTodos).toHaveLength(1);
+        });
+
 
     });
 
